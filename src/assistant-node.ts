aafa9fb80d3bf73622470622770a0abfadd1a25a
Han Tuerker <han.tuerker@gmail.com>
2024-04-18 23:38:03 +0300

    fix type

--- src/assistant-node.ts
+++ src/assistant-node.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Node, ClaudeTool, ClaudeRequest, ClaudeResponse } from "./index.js";
+import { Node, ClaudeTool, ClaudeRequest, ClaudeResponse, ClaudeMessage } from "./index.js";
 
 const nodeToClaudeTool: (node: Node) => ClaudeTool = (node) => {
   return {
