3d6f8198cfd53e86579c02ffffa9ffd118cb0103
Han Tuerker <han.tuerker@gmail.com>
2024-04-18 23:36:26 +0300

    complete assistant, ready to test

--- src/assistant-node.ts
+++ src/assistant-node.ts
@@ -1,91 +1,33 @@
 import axios from "axios";
+import { Node, ClaudeTool, ClaudeRequest, ClaudeResponse } from "./index.js";
 
-import fs from "fs";
-import { jsonc } from "jsonc/lib/jsonc";
-
-type Node = {
-  label: string;
-  meta: {
-    description?: string;
-  };
-  inputs: Record<string, {
-    type: string;
-    description: string;
-  }>;
-}
-
-type ClaudeTool = {
-  name: string;
-  description: string;
-  input_schema: {
-    type: string;
-    properties: Record<string, {
-      type: string;
-      description: string;
-    }>;
-  };
-}
-
-const nodeToClaudeTool = (node: Node): ClaudeTool => {
+const nodeToClaudeTool: (node: Node) => ClaudeTool = (node) => {
   return {
-    name: node.label,
+    // Use node.meta.id as the name of the tool. Spaces are not allowed.
+    name: node.meta.id,
     description: node.meta.description ?? "",
     input_schema: {
       type: "object",
-      properties: node.inputs,
-      // TODO: required fields
-      // required: Object.keys(node.inputs),
+      properties: Object.entries(node.inputs.properties)
+        .reduce((properties, [name, value]) => ({
+          ...properties, [name]: {
+            type: value.type,
+            enum: value.enum,
+            description: value.description
+          }
+        }), {}),
+      required: node.inputs.required ?? [],
     },
   };
-
 }
 
-type ClaudeResponse = {
-  // "id": "msg_01MTjTLjDwbD5TRd8KAzmxFw",
-  "id": string,
-  // "type": "message",
-  "type": "message",
-  // "role": "assistant",
-  "role": "assistant",
-  // "model": "claude-3-opus-20240229",
-  "model": string,
-  "stop_sequence": null,
-  // "usage": { "input_tokens": 527, "output_tokens": 129 },
-  "usage": { "input_tokens": number, "output_tokens": number },
-  // "content": [
-  //   {
-  //     "type": "text",
-  //     "text": "<thinking>\nTo get the weather for a location, the relevant tool is get_weather. Let's look at the required parameters:\n\nlocation: The user provided \"San Francisco\", so we have the location.\n\nunit: This is not a required parameter, so we can proceed without it. The weather can be returned in the default units.\n</thinking>"
-  //   },
-  //   {
-  //     "type": "tool_use",
-  //     "id": "toolu_01McqgbVaxELGfAvVwFFpFdT",
-  //     "name": "get_weather",
-  //     "input": { "location": "San Francisco, CA" }
-  //   }
-  // ],
-  "content": string | Array<{
-    type: "text",
-    "text": string
-  } | {
-    type: "tool_use",
-    "id": string,
-    name: "string",
-    input: Record<string, string>
-  } | {
-    type: "tool_result",
-    tool_use_id: string,
-    content: string
-  }>,
-  "stop_reason": "tool_use" | "stop_sequence" | "end_turn"
-}
 export default async function assistant(
-  { claudeApiKey, nodes }: { claudeApiKey: string, nodes?: Node[] },
-  { logging, execute }: {
-    logging: { log: (...message: any[]) => void },
-    execute: (name: string, input: Record<string, any>) => Promise<any>
-  }
+  { claudeApiKey, nodes, model, maxTokens, userPrompt, systemPrompt, messages }:
+    { claudeApiKey: string, nodes: Node[], model: string, maxTokens: number, userPrompt: string, systemPrompt?: string, messages?: ClaudeMessage[] },
+  { logging, execute }: any
 ) {
+  const version = "2023-06-01";
+  const beta = "tools-2024-04-04";
 
   const client = axios.create({
     baseURL: "https://api.anthropic.com/v1",
@@ -93,77 +35,72 @@ export default async function assistant(
       'Accept': 'application/json',
       'Content-Type': 'application/json',
       'x-api-key': claudeApiKey,
-      'anthropic-version': '2023-06-01',
-      'anthropic-beta': 'tools-2024-04-04'
+      'anthropic-version': version,
+      'anthropic-beta': beta
     }
   });
 
   const tools = nodes?.map(nodeToClaudeTool) ?? []
-  logging.log(tools);
 
+  const initialMessages = [
+    ...(messages ?? []),
+    {
+      "role": "user",
+      "content": userPrompt,
+    }];
 
   const baseRequest = {
-    "model": "claude-3-opus-20240229",
-    "max_tokens": 1024,
+    "model": model,
+    "max_tokens": maxTokens,
+    "system": systemPrompt || "",
     "tools": tools,
-    "messages": [{
-      "role": "user",
-      "content": "What is the weather like in Bursa, Turkey in fahrenheit?"
-    }]
-  } as {
-    model: string,
-    max_tokens: number,
-    tools: ClaudeTool[],
-    messages: {
-      role: "user" | "assistant",
-      content: ClaudeResponse["content"]
-    }[]
-  }
+    "messages": initialMessages
+  } as ClaudeRequest;
 
-  let nextRequest = { ...baseRequest };
-  let currentResponse = await client.post("/messages", nextRequest);
-  if (currentResponse.status !== 200) {
-    throw currentResponse;
-  }
+  try {
+    let request = { ...baseRequest };
+    let response = await client.post("/messages", request);
 
-  let currentData = currentResponse.data as ClaudeResponse;
-
-  logging.log("current data:", currentData);
-
-  if (currentData.stop_reason === "tool_use") {
-    nextRequest = {
-      ...nextRequest,
-      messages: [
-        ...nextRequest.messages, {
-          role: "assistant",
-          content: currentData.content
-        }]
-    }
-    if (typeof currentData.content === "object") {
-      const toolsToExecute = currentData.content.filter(content => content.type === "tool_use")
-      for (const tool of toolsToExecute) {
-        const toolOutput = await execute(tool.name, tool.input);
-        nextRequest = {
-          ...nextRequest,
-          messages: [
-            ...nextRequest.messages, {
-              role: "user",
-              content: [{
-                type: "tool_result",
-                tool_use_id: tool.id,
-                content: toolOutput
-              }]
-            }]
+    do {
+      if (response.status !== 200) {
+        if (response.data.type === "error") {
+          throw response.data.error;
         }
-        logging.log("next request: ", nextRequest);
-        currentResponse = await client.post("/messages", nextRequest);
-        if (currentResponse.status !== 200) {
-          // throw currentResponse;
-        }
-        logging.log("current response: ", currentResponse);
-        currentData = currentResponse.data as ClaudeResponse;
-        logging.log("current data:", currentData);
+        throw response;
       }
+      let result = response.data as ClaudeResponse;
+      const content = result.content;
+
+      const isToolUse = result.stop_reason === "tool_use" && content instanceof Array;
+      if (isToolUse) {
+        request.messages.push({
+          role: "assistant",
+          content
+        });
+        const toolUses = content.filter(content => content.type === "tool_use");
+        for (const toolUse of toolUses) {
+          const tool = tools.find(tool => tool.name === toolUse.name);
+          const node = nodes?.find(node => node.meta.id === toolUse.name);
+          if (!tool || !node) {
+            throw new Error(`Unknown tool: ${toolUse}`);
+          }
+          request.messages.push({
+            role: "user",
+            content: [{
+              type: "tool_result",
+              tool_use_id: toolUse.id,
+              content: await execute(node.meta.name, toolUse.input)
+            }]
+          });
+        }
+      }
+
+      response = await client.post("/messages", request);
+    } while (response && response.data && response.data.stop_reason !== "end_turn");
+    return {
+      data: response.data
     }
+  } catch (error) {
+    return { error }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,134 @@
 import os from "os";
 import fs from "fs/promises";
-import ReadLine from 'readline';
+import ReadLine from 'readline/promises';
 import { jsonc } from "jsonc/lib/jsonc"
 
 import assistant from './assistant-node';
 
 import 'dotenv/config';
+
+export type Node = {
+  meta: {
+    id: string;
+    description: string;
+    name: string;
+    [key: string]: any;
+  };
+  inputs: {
+    type: string;
+    required: string[];
+    properties: Record<string, {
+      description: string;
+      [key: string]: any;
+    }>;
+  };
+  [key: string]: any;
+};
+export type ClaudeTool = {
+  name: string;
+  description: string;
+  input_schema: {
+    type: string;
+    properties: Record<string, {
+      type: string;
+      description: string;
+      enum?: string[];
+    }>;
+    required: string[];
+  };
+};
+
+export type ClaudeTextContent = {
+  type: "text";
+  text: string;
+};
+
+export type ClaudeToolUseContent = {
+  type: "tool_use",
+  id: string,
+  name: string,
+  input: Record<string, string>
+};
+
+export type ClaudeToolResultContent = {
+  type: "tool_result",
+  tool_use_id: string,
+  content: string
+};
+
+export type ClaudeMessage = {
+  role: "user" | "assistant",
+  content: ClaudeResponse["content"]
+};
+
+export type ClaudeRequest = {
+  "model": string,
+  "max_tokens": number,
+  "tools": ClaudeTool[],
+  "messages": ClaudeMessage[]
+};
+
+export type ClaudeResponse = {
+  "id": string,
+  "type": "message",
+  "role": "assistant",
+  "model": string,
+  "stop_sequence": null,
+  "usage": { "input_tokens": number, "output_tokens": number },
+  "content": string | ClaudeTextContent[] | ClaudeToolUseContent[] | ClaudeToolResultContent[];
+  "stop_reason": "tool_use" | "stop_sequence" | "end_turn"
+};
+
+
+const testNodes = [
+  {
+    "onFail": null,
+    "label": "Get weather",
+    "script": "export default async ({\n    location,\n    unit\n}, {\n    logging\n}) => {\n    // API call to fetch weather based on location and unit\n    logging.log(`Weather info for ${location} in ${unit} unit`);\n    return `Weather info for ${location} in ${unit} unit`;\n}",
+    "output": {
+      "buildship": {},
+      "description": "Weather information for the specified location and unit",
+      "title": "Weather Info",
+      "type": "string"
+    },
+    "type": "script",
+    "id": "1ad17f18-3cb1-4422-859a-8db157bfc629",
+    "inputs": {
+      "required": ["location"],
+      "properties": {
+        "location": {
+          "buildship": { "toBeAutoFilled": true, "index": 0 },
+          "description": "The city and state, e.g. San Francisco, CA",
+          "title": "Location",
+          "type": "string"
+        },
+        "unit": {
+          "description": "The unit of temperature, either \"celsius\" or \"fahrenheit\" use celcius by default",
+          "title": "Unit",
+          "buildship": {
+            "index": 1,
+            "sensitive": false,
+            "options": [
+              { "value": "celsius", "label": "Celsius" },
+              { "label": "Fahrenheit", "value": "fahrenheit" }
+            ],
+            "toBeAutoFilled": true
+          },
+          "enum": ["celsius", "fahrenheit"],
+          "pattern": "",
+          "type": "string"
+        }
+      },
+      "type": "object"
+    },
+    "meta": {
+      "description": "Get the current weather in a given location with specified unit",
+      "id": "1ad17f18-3cb1-4422-859a-8db157bfc629",
+      "name": "Get weather"
+    },
+    "description": "Get the current weather in a given location"
+  }
+] as Node[];
 
 const main = async () => {
 
@@ -15,38 +138,28 @@ const main = async () => {
   const logging = {
     log: (...message: any[]) => {
       console.log(jsonc.beautify(jsonc.stringify(message)));
-      fs.writeFile("./log.txt", jsonc.stringify(message) + os.EOL, { flag: "a" });
+      fs.writeFile("./log.txt", os.EOL + jsonc.stringify(message) + os.EOL, { flag: "a" });
     },
   }
   await assistant({
     claudeApiKey: process.env.CLAUDE_API_KEY,
-    nodes: [{
-      label: "get_weather",
-      meta: {
-        description: "Get the current weather in a given location",
-      },
-      inputs: {
-        location: {
-          type: "string",
-          description: "The city and state, e.g. San Francisco, CA",
-        },
-        unit: {
-          type: "string",
-          description: 'The unit of temperature, either celsius or fahrenheit',
-        },
-      },
-    }]
+    nodes: testNodes,
+    model: "claude-3-opus-20240229",
+    maxTokens: 1024,
+    messages: undefined,
+    systemPrompt: "Respond only in Turkish.",
+    userPrompt: "What is the current weather in Bursa, Turkey?",
   }, {
     logging,
     execute: async (name: string, input: Record<string, any>) => {
-      if (name === "get_weather") {
+      const node = testNodes.find(node => node.meta.name === name);
+      if (node) {
         if (!input.location) {
           throw new Error("Missing location");
         }
         if (input.unit && !["celsius", "fahrenheit"].includes(input.unit)) {
           throw new Error("Invalid unit");
         }
-        console.log(`Getting weather for ${input.location} in ${input.unit ?? "celcius"}...`);
         if (input.unit === "fahrenheit") {
           return "70°F";
         }
@@ -54,13 +167,11 @@ const main = async () => {
       }
       throw new Error(`Unknown tool: ${name}`);
     }
-  });
+  }).then(console.log);
 
 }
-const readLine = ReadLine.createInterface({ input: process.stdin, output: process.stdout });
+export const readLine = ReadLine.createInterface({ input: process.stdin, output: process.stdout });
 
-readLine.question("Press any key to continue", () => {
-  main()
-    .then(() => console.log("ok"))
-    .catch(console.error)
-});
+main()
+  .then(() => console.log("ok"))
+  .catch(console.error);
